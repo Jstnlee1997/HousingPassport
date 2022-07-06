86b4a853747908a10f5b607c185b49a554d53d7b
Justin Lee <jstnlee1997@gmail.com>
2022-07-06 17:01:48 +0100

    Reroute users/ to index page to show epc certificate

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,34 @@
-var express = require("express");
-var router = express.Router();
+const router = require("express").Router();
+const { getLmkKeyOfAddress, addCertificateByLmkKey } = require("./epc");
+const { getCertificateByLmkKey } = require("./dynamo-certs");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+router
+  .route("/")
+  .get((req, res, next) => {
+    res.render("index", { title: "Express" });
+  })
+  // user has identified their address
+  .post(async (req, res, next) => {
+    // obtain selected address
+    const address = req.body.address;
+
+    /* TODO: save in table userId and their address */
+
+    // TODO: Get lmk-key using user's address and store into user database
+
+    // Return lmk-key of address
+    const lmkKey = await getLmkKeyOfAddress(address);
+
+    /* TODO: Check if certificate exists in database, add in if it is not, else just retrieve it */
+    const found = await getCertificateByLmkKey(lmkKey);
+    if (found.Item) {
+      console.log("Certificate of this address is present in database");
+      res.json(found.Item);
+    } else {
+      console.log("Certificate of this address not present in database");
+      res.json(await addCertificateByLmkKey(lmkKey));
+    }
+  });
 
 module.exports = router;
--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -7,33 +7,9 @@ const {
 const { getCertificateByLmkKey } = require("./dynamo-certs");
 
 /* GET users listing. */
-router
-  .route("/")
-  .get((req, res) => {
-    res.send("respond with a resource");
-  })
-  // user has identified their address
-  .post(async (req, res, next) => {
-    // obtain selected address
-    const address = req.body.address;
-
-    /* TODO: save in table userId and their address */
-
-    // TODO: Get lmk-key using user's address and store into user database
-
-    // Return lmk-key of address
-    const lmkKey = await getLmkKeyOfAddress(address);
-
-    /* TODO: Check if certificate exists in database, add in if it is not, else just retrieve it */
-    const found = await getCertificateByLmkKey(lmkKey);
-    if (found.Item) {
-      console.log("Certificate of this address is present in database");
-      res.json(found.Item);
-    } else {
-      console.log("Certificate of this address not present in database");
-      res.json(await addCertificateByLmkKey(lmkKey));
-    }
-  });
+router.route("/").get((req, res) => {
+  res.send("respond with a resource");
+});
 
 // routing for new users to input postcode
 router.route("/new").get((req, res) => {
