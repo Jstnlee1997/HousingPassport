da03946dd8bf2768a5e17ebc91e54e8723760f94
Justin Lee <jstnlee1997@gmail.com>
2022-07-19 17:46:16 +0100

    Create API for government to access aggregated data of local authorities

--- myapp/routes/government.js
+++ myapp/routes/government.js
@@ -1,7 +1,20 @@
+const { getAggregateDataOfLocalAuthority } = require("./dynamo-aggregate");
+
 const router = require("express").Router();
 
-router.route("/").get((req, res, next) => {
-  res.render("register");
+router.route("/:localAuthority").get((req, res, next) => {
+  // Get the aggregated data of this localAuthority
+  getAggregateDataOfLocalAuthority(req.params.localAuthority).then(
+    async (result) => {
+      if (result.Item) {
+        res.json(result.Item);
+      } else {
+        res.send(
+          "There are currently no properties under this local-authority"
+        );
+      }
+    }
+  );
 });
 
 module.exports = router;
